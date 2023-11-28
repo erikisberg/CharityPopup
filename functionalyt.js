d6ab5b361331cb2f366607a50d1283a47ea6d033
Erik Isberg <erik@isberg.nu>
2023-11-28 13:37:44 +0100

    Update functionalyt.js

--- functionalyt.js
+++ functionalyt.js
@@ -4,7 +4,7 @@
             <img src="https://uploads-ssl.webflow.com/5d9cc2c7b5891d21c8901fd2/5df520e1b0ebe722d03f9f82_dios_logo_cmyk_vit.png" style="width: 100px; height: 100px; cursor: pointer;">
         </div>
         <div id="charity-overlay" style="display:none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 1001;">
-            <div id="charity-popup" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; max-width: 600px; padding: 20px;">
+            <div id="charity-popup" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; max-width: 600px; padding: 20px; font-family: 'Open Sans', sans-serif;">
                 <img src="image_url">
                 <h1>Headline</h1>
                 <p>Paragraph</p>
